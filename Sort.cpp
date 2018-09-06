db06498e4b4e690670ac04f2fda139b1e2f8c7c1
k997 <k979569847@163.com>
2018-09-06 11:18:32 +0800

    modify the notes of Sort in English

--- Sort.cpp
+++ Sort.cpp
@@ -5,14 +5,16 @@ using namespace std;
 void selectSort(int a[], int len){
 /*
 选择排序，之前的保持有序，选择之后最小的插入有序部分之后 
+selectSort, keep the former is orderd, and find the minimun element from the latter insert orderd part
 */
 	int n = sizeof(a)/sizeof(int);
-	printf("%d %d %d\n",sizeof(a),sizeof(a[0]),n);//结果为8，4，2
+	printf("%d %d %d\n",sizeof(a),sizeof(a[0]),n);//The result is 8，4，2
+	// The array will be regarded as pointer and miss its length of array, so the length of array(parameter len) is necesssary;
 	//函数中传递数组，数组会被当成指针，丢失数组长度 ，所以必须传入数组长度 
 
 	for(int i=0;i<len;i++){
 		
-		int k=i;//最小元素数组下标 
+		int k=i;//the index of minium element 
 		for(int j=i;j<len;j++){
 			if(a[k]>a[j]) k=j;
 		}
@@ -46,7 +48,7 @@ int main(){
 	}
 	puts("\n");
 	sort(a,a+len,cmp);
-	/*sort函数，注意using namespace std;和cmp函数*/ 
+	/*if use sort funciton, pay attention to "using namespace std;", "#include <algorithm>" and cmp funciton*/ 
 	for(int i=0; i< len; i++){
 		printf("%d ",a[i]);
 	}
